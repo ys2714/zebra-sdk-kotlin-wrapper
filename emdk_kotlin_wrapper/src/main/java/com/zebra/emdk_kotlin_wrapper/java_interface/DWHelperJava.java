96d8e2ef241cd9659083b9a7bb8f3b5793a2ddac
Sun <YS2714@zebra.com>
2025-11-27 17:57:31 +0900

    make DWHelperJava methods public

--- emdk_kotlin_wrapper/src/main/java/com/zebra/emdk_kotlin_wrapper/java_interface/DWHelperJava.java
+++ emdk_kotlin_wrapper/src/main/java/com/zebra/emdk_kotlin_wrapper/java_interface/DWHelperJava.java
@@ -12,100 +12,100 @@ import kotlin.Unit;
 
 public class DWHelperJava {
 
-    void addScanDataListener(DataWedgeHelper.ScanDataListener listener) {
+    public static void addScanDataListener(DataWedgeHelper.ScanDataListener listener) {
         DataWedgeHelper.INSTANCE.addScanDataListener(listener);
     }
 
-    void removeScanDataListener(DataWedgeHelper.ScanDataListener listener) {
+    public static void removeScanDataListener(DataWedgeHelper.ScanDataListener listener) {
         DataWedgeHelper.INSTANCE.removeScanDataListener(listener);
     }
 
-    void prepare(Context context, BooleanCompletion callback) {
+    public static void prepare(Context context, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.prepare(context, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void checkDWStatus(Context context, BooleanCompletion callback) {
+    public static void checkDWStatus(Context context, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.checkDWStatus(context, (enabled) -> {
             callback.onComplete(enabled);
             return Unit.INSTANCE;
         });
     }
 
-    void enableDW(Context context, BooleanCompletion callback) {
+    public static void enableDW(Context context, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.enableDW(context, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void disableDW(Context context, BooleanCompletion callback) {
+    public static void disableDW(Context context, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.disableDW(context, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void createProfile(Context context, String name, BooleanCompletion callback) {
+    public static void createProfile(Context context, String name, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.createProfile(context, name, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void deleteProfile(Context context, String name, BooleanCompletion callback) {
+    public static void deleteProfile(Context context, String name, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.deleteProfile(context, name, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void bindProfileToApp(Context context, String name, String packageName, BooleanCompletion callback) {
+    public static void bindProfileToApp(Context context, String name, String packageName, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.bindProfileToApp(context, name, packageName, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void configBarcodePlugin(Context context, String name, boolean enable, boolean hardTrigger, BooleanCompletion callback) {
+    public static void configBarcodePlugin(Context context, String name, boolean enable, boolean hardTrigger, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.configBarcodePlugin(context, name, enable, hardTrigger, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void configKeystrokePlugin(Context context, String name, boolean enable, BooleanCompletion callback) {
+    public static void configKeystrokePlugin(Context context, String name, boolean enable, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.configKeystrokePlugin(context, name, enable, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void configIntentPlugin(Context context, String name, BooleanCompletion callback) {
+    public static void configIntentPlugin(Context context, String name, BooleanCompletion callback) {
         DataWedgeHelper.INSTANCE.configIntentPlugin(context, name, (success) -> {
             callback.onComplete(success);
             return Unit.INSTANCE;
         });
     }
 
-    void softScanTrigger(Context context, DWAPI.SoftScanTriggerOptions option) {
+    public static void softScanTrigger(Context context, DWAPI.SoftScanTriggerOptions option) {
         DataWedgeHelper.INSTANCE.softScanTrigger(context, option);
     }
 
-    DWScannerMap.DWScannerInfo getScannerInfo(Context context, String id) {
+    public static DWScannerMap.DWScannerInfo getScannerInfo(Context context, String id) {
         return DataWedgeHelper.INSTANCE.getScannerInfo(context, id);
     }
 
-    void getScannerStatus(Context context, int delaySeconds, GeneralCompletion<DWAPI.ScannerStatus> callback) {
+    public static void getScannerStatus(Context context, int delaySeconds, GeneralCompletion<DWAPI.ScannerStatus> callback) {
         DataWedgeHelper.INSTANCE.getScannerStatus(context, delaySeconds, (status) -> {
             callback.onComplete(status);
             return Unit.INSTANCE;
         });
     }
 
-    void getScannerList(Context context, GeneralCompletion<List<DWScannerMap.DWScannerInfo>> callback) {
+    public static void getScannerList(Context context, GeneralCompletion<List<DWScannerMap.DWScannerInfo>> callback) {
         DataWedgeHelper.INSTANCE.getScannerList(context, (list) -> {
             callback.onComplete(list);
             return Unit.INSTANCE;
