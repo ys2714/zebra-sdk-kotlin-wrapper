73240ca27155b43a0ffb7738f985832151a0b16a
Sun <YS2714@zebra.com>
2026-04-01 14:19:00 +0900

    add IDLE status

--- emdk_kotlin_wrapper/src/main/java/com/zebra/emdk_kotlin_wrapper/dw/DWAPI.kt
+++ emdk_kotlin_wrapper/src/main/java/com/zebra/emdk_kotlin_wrapper/dw/DWAPI.kt
@@ -154,7 +154,7 @@ object DWAPI {
     @Keep
     enum class ScannerStatus(val value: String) {
         UNKNOWN("UNKNOWN"),
-        IDLE("IDLE"),
+        IDLE("IDLE"), // Scanner is idle
         WAITING("WAITING"), // Scanner is ready to be triggered
         SCANNING("SCANNING"), // Scanner is emitting a scanner beam
         DISABLED("DISABLED"), // Scanner is disabled
